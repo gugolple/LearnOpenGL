2d6a299bb863f2c1cab343f1c820a1c1d4883323
gugolple <juanignacio.croce@gmail.com>
2024-10-13 18:12:00 +0200

    Temporary checkpoint. Solved the issue of lost shaders due to copy and deletion instead of moving. Now is no longer rendering

---
+++ chapters/6.0_shaders/src/simple_drawable.cpp
@@ -0,0 +1,50 @@
+#include "simple_drawable.hpp"
+#include "math.h"
+
+SimpleDrawable::SimpleDrawable(std::vector<float> &&vertices,
+		std::vector<unsigned int> &&indices,
+		unsigned int numVerts,
+		ShaderProgram &&shaderProgram):
+	vertices(vertices), indices(indices), shaderProgram(std::move(shaderProgram)), numVerts(numVerts) {
+	assert(indices.size()%3==0);
+    	glGenVertexArrays(1, &this->VAO);
+    	glGenBuffers(1, &this->VBO);
+    	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+    	glBindVertexArray(this->VAO);
+
+    	glBindBuffer(GL_ARRAY_BUFFER, this->VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(VERTICES_TYPE)*this->vertices.size(), this->vertices.data(), GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, this->numVerts, GL_FLOAT, GL_FALSE, this->numVerts * sizeof(float), (void*)0);
+    	glEnableVertexAttribArray(0);
+
+    	// You can unbind the this->VAO afterwards so other this->VAO calls won't accidentally modify this this->VAO, but this rarely happens. Modifying other
+    	// this->VAOs requires a call to glBindVertexArray anyways so we generally don't unbind this->VAOs (nor this->VBOs) when it's not directly necessary.
+    	// glBindVertexArray(0);
+
+
+    	// bind the this->VAO (it was already bound, but just to demonstrate): seeing as we only have a single this->VAO we can 
+    	// just bind it beforehand before rendering the respective triangle; this is another approach.
+    	glBindVertexArray(this->VAO);
+};
+
+SimpleDrawable::~SimpleDrawable() {
+	// Release buffers
+	glDeleteBuffers(1, &this->VBO);
+	// Release vertex array
+	GLuint arr[2];
+	arr[0] = this->VAO;
+	glDeleteVertexArrays(1, arr);
+}
+
+void SimpleDrawable::render() {
+	glUseProgram(this->shaderProgram.getShaderProgram());
+	glBindVertexArray(this->VAO);
+
+        // render the triangle
+        glDrawArrays(GL_TRIANGLES, 0, this->numVerts);
+}		
+
+const ShaderProgram& SimpleDrawable::getShaderProgram(){
+	return this->shaderProgram;
+}
---
+++ chapters/6.0_shaders/src/simple_drawable.hpp
@@ -0,0 +1,33 @@
+#ifndef SIMPLE_DRAWABLE_HPP
+#define SIMPLE_DRAWABLE_HPP
+
+#include "shader_handler.hpp"
+
+// OpenGL specifics
+#include <glad.h>
+#include <GLFW/glfw3.h>
+
+#define VERTICES_TYPE float
+#define INDICES_TYPE unsigned int 
+
+class SimpleDrawable {
+	public:
+		SimpleDrawable(std::vector<VERTICES_TYPE> &&vertices,
+				std::vector<INDICES_TYPE> &&indices,
+				unsigned int numVerts,
+				ShaderProgram &&shaderProgram);
+		~SimpleDrawable();
+		void render();
+		const ShaderProgram& getShaderProgram();
+	private:
+		ShaderProgram shaderProgram;
+		unsigned int VBO;
+		unsigned int VAO;
+
+		const unsigned int numVerts;
+		const std::vector<VERTICES_TYPE> vertices;
+		// We start from 0 and blocks of 3 numbers, because we draw triangles
+		const std::vector<INDICES_TYPE> indices;
+};
+
+#endif
